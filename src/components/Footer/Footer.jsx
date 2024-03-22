a538f6a01e1e8888f1815dbcf1b8678ee171f298
Charles A. Best-Zambrana <c.best-zambrana@erasolutions.us>
2024-03-22 12:25:00 -0400

    second commi

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -72,12 +72,12 @@ const Footer = () => {
           <Col lg="3" md="4" sm="6">
             <div className="mb-4">
               <h5 className="footer__link-title mb-4">Head Office</h5>
-              <p className="office__info">123 Zindabazar, Sylhet, Bangladesh</p>
+              <p className="office__info">Seaman St, West Hartford, CT</p>
               <p className="office__info">Phone: +0995345875365</p>
 
-              <p className="office__info">Email: muhib5532@gmail.com</p>
+              <p className="office__info">Email: carrentalus@support.com</p>
 
-              <p className="office__info">Office Time: 10am - 7pm</p>
+              <p className="office__info">Office Time: 9am - 6pm</p>
             </div>
           </Col>
 
